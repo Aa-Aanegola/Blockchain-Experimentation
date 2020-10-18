e28c27a5b764cb70cfddc601694a04c968b300fe
Aakash-AA <aakash.aanegola@gmail.com>
2020-10-18 15:17:59 +0530

    Added Encryption directory

---
+++ bc/Encryption/RSA.py
@@ -0,0 +1,57 @@
+import math
+import random
+
+
+def millerRabin(n, d):
+    
+    a = 2 + random.randint(1, n-4)
+    
+    x = pow(a, d, n)
+    
+    if x == 1 or x == n-1:
+        return True
+    
+    while d != n-1:
+        x = (x*x)%n
+        d *= 2
+        
+        if x == 1:
+            return False
+        if x == n-1:
+            return True
+        
+    return False
+
+
+def isPrime(n, k):
+    
+    if n <= 1 or n == 4:
+        return False
+    if n <= 3:
+        return True
+    
+    d = n-1
+    while d%2 == 0:
+        d //= 2
+    
+    for i in range(0, k):
+        if millerRabin(n, d) == False:
+            return False
+    
+    return True
+
+
+def generateKey(len):
+    lowerBound = 2 << (len-1)
+    upperBound = 2 << (len)
+
+    p, q = -1, -1
+    
+    while p == -1:
+        randNum = random.randint(lb+1, ub-1)
+        if isPrime(randNum):
+            p = randNum
+    while q == -1:
+        randNum = random.randint(lb+1, ub-1)
+        if isPrime(randNum) and randNum != p:
+            q = randNum
